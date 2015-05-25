31ec7f7ebcff239031612f743f63039c1e83a9cc
Niels Richard Hansen <Niels.R.Hansen@math.ku.dk>
2015-05-25 23:07:28 +0200

    Added package documentation

---
+++ R/tablorg-package.R
@@ -0,0 +1,14 @@
+##' tablorg: Table organization of computations
+##' 
+##' Tools for organizing results from R computations. Overview and easy access 
+##' to the results are provided via an HTML table. This is a light weight alternative 
+##' to an actual database. The tools are useful when running many analyses or simulations
+##' with varying parameter settings. The results are organized in directories 
+##' and accessed through an HTML table via a web server. This gives an R independent
+##' way of monitoring computations running e.g. as an R batch script either 
+##' locally or on a remote server. 
+##' 
+##' @author Niels Richard Hansen \email{Niels.R.Hansen@@math.ku.dk}
+##' @docType package
+##' @name tablorg
+NULL
